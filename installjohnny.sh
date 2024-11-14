bec2db99c63fd5e9e99e9d1404dbbe2d2ffceabb
JohnnyLove777 <47362296+JohnnyLove777@users.noreply.github.com>
2024-11-14 20:23:35 -0300

    docker install

--- installjohnny.sh
+++ installjohnny.sh
@@ -65,11 +65,22 @@ function instalar_evolution_api_johnnyzap {
     cd ~ || exit
     echo "Diretório atual: $(pwd)"
 
-    # Verificar se Docker já está instalado
+    # Instalação do Docker e Docker Compose
     if ! command -v docker &> /dev/null; then
         echo "Docker não encontrado. Instalando Docker e Docker Compose..."
+        
+        # Adicionar repositório do Docker
         sudo apt update
-        sudo apt install -y docker-ce docker-compose
+        sudo apt install -y ca-certificates curl gnupg lsb-release
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        
+        # Instalar Docker Engine e Docker Compose
+        sudo apt update
+        sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
         checar_status "Falha ao instalar Docker e Docker Compose."
     else
         echo "Docker já está instalado. Pulando a instalação."
